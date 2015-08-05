2f654b0abf4ca61004cbc73c1638146338a09dca
Hernan Y.Ke <yuriqiao@gmail.com>
2015-08-05 16:17:44 -0400

    Day 1

---
+++ blat/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from blat.models import Blat
+
+# Register your models here.
+class BlatAdmin(admin.ModelAdmin):
+    list_display = ('text', 'create_on', 'total_likes')
+    list_filter = ['create_on']
+    search_fields = ['text']
+
+admin.site.register(Blat, BlatAdmin)
